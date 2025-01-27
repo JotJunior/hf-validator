c0fc313a8e65bb0f444cea51a8a28ffbf2f58adf
Joao G Zanon Jr <Joao Zanon>
2025-01-27 18:36:12 -0300

    Create Length validator

--- src/Validator/Unique.php
+++ src/Validator/Unique.php
@@ -22,30 +22,35 @@ class Unique extends AbstractAttribute implements ValidatorInterface
         $this->queryBuilder = $container->get(QueryBuilder::class);
     }
 
-    /**
-     * Validates the given value against a specific index and field in the query builder.
-     *
-     * @param mixed $value The value to validate, which can be an object or any other type.
-     * @return bool Returns true if the value exists in the specified index and field, otherwise false.
-     */
     public function validate(mixed $value): bool
     {
-        if (is_object($value) && !method_exists($value, 'getId')) {
-            $this->errors[] = 'The given value is not a valid Entity object.';
+        $value = $this->extractValueId($value);
+
+        if (!$value) {
             return false;
         }
-        if (is_object($value) && method_exists($value, 'getId')) {
-            $value = $value->getId();
-        }
-        $isValid = $this->queryBuilder
-                ->from($this->index)
-                ->where($this->field, $value)
-                ->count() === 0;
 
-        if (!$isValid) {
+        $valueCount = $this->queryBuilder
+            ->from($this->index)
+            ->where($this->field, $value)
+            ->count();
+
+        $isUnique = $valueCount === 0;
+
+        if (!$isUnique) {
             $this->errors[] = 'The given value is already in use.';
         }
 
-        return $isValid;
+        return $isUnique;
+    }
+
+    private function extractValueId(mixed $value): mixed
+    {
+        if (is_object($value) && !method_exists($value, 'getId')) {
+            $this->errors[] = 'The given value is not a valid Entity object.';
+            return null;
+        }
+
+        return is_object($value) ? $value->getId() : $value;
     }
 }
