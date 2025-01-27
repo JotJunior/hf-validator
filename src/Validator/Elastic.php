74eb6dba5e2b357ef8d8dae5fe0288f64263da8a
Joao G Zanon Jr <Joao Zanon>
2025-01-26 23:03:34 -0300

    Fix Elastic validation

--- src/Validator/Elastic.php
+++ src/Validator/Elastic.php
@@ -11,18 +11,26 @@ use Jot\HfValidator\ValidatorInterface;
 class Elastic extends AbstractAttribute implements ValidatorInterface
 {
 
-    public function __construct(protected string $index, protected string $field, protected QueryBuilder $queryBuilder)
+    public function __construct(protected string $index, protected string $field, protected ?QueryBuilder $queryBuilder = null)
     {
     }
 
+
     /**
-     * Validates whether the given value exists in the specified field of the index.
+     * Validates the given value against a specific index and field in the query builder.
      *
-     * @param string $value The value to be validated.
-     * @return bool Returns true if the value exists, false otherwise.
+     * @param mixed $value The value to validate, which can be an object or any other type.
+     * @return bool Returns true if the value exists in the specified index and field, otherwise false.
      */
     public function validate(mixed $value): bool
     {
+        if (is_object($value) && !method_exists($value, 'getId')) {
+            $this->errors[] = 'The given value is not a valid Entity object.';
+            return false;
+        }
+        if (is_object($value) && method_exists($value, 'getId')) {
+            $value = $value->getId();
+        }
         return $this->queryBuilder
                 ->from($this->index)
                 ->where($this->field, $value)
